f90d97667d8fb97577396b7e1f48fb451ef7be4b
rybd111 <zhang93414@126.com>
2020-08-16 17:39:53 +0800

    updated

---
+++ KS_1.0.8/src/com/h2/backupData/pullFileFromSensor.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.h2.backupData;
+
+/**
+ * @author Hanlin Zhang
+ */
+public class pullFileFromSensor {
+
+	private String timeString=" ";
+	
+	/**
+	 * timeString must be yyyy-mm-ddhh:mm:ss
+	 * @param timeString
+	 */
+	public pullFileFromSensor(String timeString) {
+		this.timeString = timeString;
+		
+		//compare different files in each sensor's drive.
+		
+	}
+	/**
+	 * @param args
+	 * @author Hanlin Zhang.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
